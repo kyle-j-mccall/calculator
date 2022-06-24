b750f4a7065c66d49501633d1997144d5c528f06
Kyle McCall <kylem147@gmail.com>
2022-06-23 20:54:51 -0700

    Set up basic addition and subtraction calculator in HTML and Js. built test for both calculators and ran tests succesfully. git status clear it

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 // import needed modules
-
+import { add } from './utils.js'
 // define and grab DOM elements
-
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const addButton = document.getElementById('add-button');
+const result = document.getElementById('result');
 // event handlers - what needs to happen?
     // logic and calculations
     // update UI
+addButton.addEventListener('click', () => {
+    const num1 = Number(input1.value);
+    const num2 = Number(input2.value);
+    result.textContent = add(num1, num2);
+})
 
+import { subtract } from './utils.js'
+
+const subtractInput1 = document.getElementById('subtract-input1');
+const subtractInput2 = document.getElementById('subtract-input2');
+const subtractButton = document.getElementById('subtract-button');
+const subtractResult = document.getElementById('subtract-result');
+
+subtractButton.addEventListener ('click', () => {
+    const num1 = Number(subtractInput1.value);
+    const num2 = Number(subtractInput2.value);
+    subtractResult.textContent = subtract(num1, num2);
+})
+
+import { multiply } from './utils.js'
+
+const multiplyInput1 = document.getElementById('multiply-input1');
+const multiplyInput2 = document.getElementById('multiply-input2');
+const multiplyButton = document.getElementById('multiply-button');
+const multiplyResult = document.getElementById('multiply-result');
+
+multiplyButton.addEventListener('click', () => {
+    const num1 = Number(multiplyInput1.value);
+    const num2 = Number(multiplyInput2.value);
+    multiplyResult.textContent =  multiply(num1, num2);
+})
--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,33 @@
 // IMPORT code to test here
 // import { add } from '../utils.js';
-
+import { add } from '../utils.js'
 const test = QUnit.test;
 
-test('test your pure functions...', (expect) => {
+test('adds two numbers', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-
+    const num1 = 3;
+    const num2 = 4;
+    const expected = 7;
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add(num1, num2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+import { subtract } from '../utils.js'
+
+test('subtracts two numbers',(expect) => {
+    //Arrange
+    const num1 = 10;
+    const num2 = 5;
+    const expected = 5;
+
+    //Act
+    const actual = subtract(num1, num2);
+
     expect.equal(actual, expected);
 });
